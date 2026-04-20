b8327768ba8d66209577ead0f6d5151ae1592724
Vijay Patil <vijaypatil100804@gmail.com>
2026-04-20 13:52:41 +0530

    Add files via upload

--- WAD!/script.js
+++ WAD!/script.js
@@ -2,9 +2,10 @@ let students = [];
 let exams = 0;
 let results = 0;
 
+let barChart, pieChart;
+
 function showSection(section) {
     document.querySelectorAll(".section").forEach(s => s.classList.add("d-none"));
-
     document.getElementById(section + "Section").classList.remove("d-none");
 }
 
@@ -17,8 +18,12 @@ function updateDashboard() {
     document.getElementById("resultCount").innerText = results;
 
     let passCount = students.filter(s => s.result === "pass").length;
+    let failCount = students.length - passCount;
+
     let percent = students.length ? Math.round((passCount / students.length) * 100) : 0;
     document.getElementById("pass").innerText = percent + "%";
+
+    updateCharts(passCount, failCount);
 }
 
 function addStudent() {
@@ -50,4 +55,41 @@ function publishResult() {
     updateDashboard();
 }
 
-updateDashboard();
+function createCharts() {
+    let barCtx = document.getElementById("barChart").getContext("2d");
+    let pieCtx = document.getElementById("pieChart").getContext("2d");
+
+    barChart = new Chart(barCtx, {
+        type: 'bar',
+        data: {
+            labels: ['Pass', 'Fail'],
+            datasets: [{
+                label: 'Students',
+                data: [0, 0]
+            }]
+        }
+    });
+
+    pieChart = new Chart(pieCtx, {
+        type: 'pie',
+        data: {
+            labels: ['Pass', 'Fail'],
+            datasets: [{
+                data: [0, 0]
+            }]
+        }
+    });
+}
+
+function updateCharts(pass, fail) {
+    barChart.data.datasets[0].data = [pass, fail];
+    barChart.update();
+
+    pieChart.data.datasets[0].data = [pass, fail];
+    pieChart.update();
+}
+
+window.onload = function () {
+    createCharts();
+    updateDashboard();
+};
